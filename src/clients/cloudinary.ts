60c0c769a3ff89efc720eafb98db1cdba3512f6e
davidnasrulloh <davidkrb52@gmail.com>
2023-11-10 12:52:02 +0700

    update readme and env example

--- src/clients/cloudinary.ts
+++ src/clients/cloudinary.ts
@@ -1,9 +1,11 @@
 import {v2 as cloudinary} from 'cloudinary';
-          
+import { config } from "dotenv";
+config();
+
 cloudinary.config({ 
-  cloud_name: 'dyursj9xj', 
-  api_key: '681835965136766', 
-  api_secret: '***************************' 
+  cloud_name: process.env.CLOUD_NAME, 
+  api_key: process.env.API_KEY, 
+  api_secret: process.env.API_SECRET 
 });
 
 export default cloudinary;
